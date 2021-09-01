e9382ce5c67330a65bff3f700e6ae2e114f8e7aa
Abhishekaaru <abhishekaaru01@gmail.com>
2021-09-01 16:31:27 +0530

    Diagnol Traversal

---
+++ DiagonalTraversalBT.cpp
@@ -0,0 +1,25 @@
+vector<int> diagonal(Node *root)
+{
+    vector<int> v;
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+
+        while (temp)
+        {
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+
+            v.push_back(temp->data);
+            temp = temp->right;
+        }
+    }
+
+    return v;
+}
