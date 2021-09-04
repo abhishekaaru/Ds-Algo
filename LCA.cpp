09ae5870ae1dcfbf6c2c8cb78fd1091ce9e593eb
Abhishekaaru <abhishekaaru01@gmail.com>
2021-09-04 13:03:13 +0530

    LCA BST

--- LCA.cpp
+++ LCA.cpp
@@ -86,6 +86,28 @@ Node* LCA1(Node* root,int n1,int n2){
     }
 }
 
+// LCA for BST
+Node *LCABST(Node *root, int n1, int n2)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+
+    if (root->data > n1 && root->data > n2)
+    {
+        return LCABST(root->left, n1, n2);
+    }
+    else if (root->data < n1 && root->data < n2)
+    {
+        return LCABST(root->right, n1, n2);
+    }
+    else
+    {
+        return root;
+    }
+}
+
 int main(){
 
     struct Node* root = new Node(1);
