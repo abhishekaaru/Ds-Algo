3385e5fe872f5cc30b3fe60f10f75caf4e8fc384
Abhishekaaru <abhishekaaru01@gmail.com>
2021-09-06 12:25:27 +0530

    Vertical Order Traversal

--- VerticalOrderPrint.cpp
+++ VerticalOrderPrint.cpp
@@ -27,6 +27,42 @@ void getVerticalOrder(Node* root,int Hdis,map<int,vector<int>> &m){
     getVerticalOrder(root->right,Hdis+1,m);
 }
 
+// GFG
+vector<int> verticalOrder(Node *root)
+{
+    vector<int> res;
+    map<int, vector<int>> mp;
+    queue<pair<Node *, int>> q;
+    q.push({root, 0});
+
+    while (!q.empty())
+    {
+        pair<Node *, int> temp = q.front();
+        Node *temp_root = temp.first;
+        int distance = temp.second;
+        mp[distance].push_back(temp_root->data);
+        q.pop();
+        if (temp_root->left)
+        {
+            q.push({temp_root->left, distance - 1});
+        }
+
+        if (temp_root->right)
+        {
+            q.push({temp_root->right, distance + 1});
+        }
+    }
+
+    for (auto it = mp.begin(); it != mp.end(); it++)
+    {
+        vector<int> ans = it->second;
+
+        for (auto x : ans)
+            res.push_back(x);
+    }
+    return res;
+}
+
 int main(){
 
     Node* root = new Node(1);
