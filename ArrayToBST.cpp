68c274fa013341bc677b8bac863c85aa382fe80a
Abhishekaaru <abhishekaaru01@gmail.com>
2021-09-11 16:08:52 +0530

    arrayToBST meathod 2

--- ArrayToBST.cpp
+++ ArrayToBST.cpp
@@ -1,11 +1,12 @@
 
 
+// METHOD 1
 Node *solve(vector<int> &nums, int st, int en)
 {
     if (st >= en){
         return NULL;
     }
-    
+
     int mid = (st + en) / 2;
 
     Node *root = new Node(nums[mid]);
@@ -14,12 +15,30 @@ Node *solve(vector<int> &nums, int st, int en)
 
     return root;
 }
+
+// Method 2 (GFG) (100% solution)
+Node *solve1(vector<int> &nums, int low, int high)
+{
+    if (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        Node *root = new Node(nums[mid]);
+        root->left = solve(nums, low, mid - 1);
+        root->right = solve(nums, mid + 1, high);
+        return root;
+    }
+    return NULL;
+}
+
+
+
 vector<int> sortedArrayToBST(vector<int> &nums)
 {
 
     vector<int> v;
 
-    Node *root = solve(nums, 0, nums.size());
+    // Node *root = solve(nums, 0, nums.size());
+    Node *root = solve1(,nums, 0, nums.size()-1);
 
     return v;
 }
