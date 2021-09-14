6080f67b85a57ca89f5a39f2a09c8d919b565085
Abhishekaaru <abhishekaaru01@gmail.com>
2021-09-14 16:01:10 +0530

    Merge K Sorted Array

--- MergeKSortedArrayHeap.cpp
+++ MergeKSortedArrayHeap.cpp
@@ -1,13 +1,71 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define pii pair<int,int>
+// Another Way (GFG)
+class ele
+{
+public:
+    int value;
+    int arrayNo;
+    int index;
 
+    ele(int v, int a, int i)
+    {
+        value = v;
+        arrayNo = a;
+        index = i;
+    }
+};
+
+struct compare
+{
+    bool operator()(ele a, ele b)
+    {
+        return a.value > b.value;
+    }
+};
+
+vector<int> mergeKArrays(vector<vector<int>> a, int k)
+{
+
+    priority_queue<ele, vector<ele>, compare> pq;
+
+    for (int i = 0; i < k; i++)
+    {
+        ele temp(a[i][0], i, 0);
+        pq.push(temp);
+    }
+
+    vector<int> ans;
+
+    while (!pq.empty())
+    {
+
+        ele x = pq.top();
+        pq.pop();
+
+        ans.push_back(x.value);
+
+        if (x.index < k - 1)
+        {
+            int array = x.arrayNo;
+            int index = x.index + 1;
+            int value = a[array][index];
+
+            ele temp(value, array, index);
+            pq.push(temp);
+        }
+    }
+
+    return ans;
+}
+
+// One Way
 int main(){
     int k;
     cin>>k;
 
-    vector<vector<int>> a(k); // 2D vector
+    vector<vector<int>> a(k);
 
     for(int i=0;i<k;i++){
 
