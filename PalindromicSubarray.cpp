e2a57e0567783085a5f993be61e0bbece9d416c1
Abhishek Kumar <abhishekaaru01@gmail.com>
2021-12-07 12:50:27 +0530

    longest palindromic

--- PalindromicSubarray.cpp
+++ PalindromicSubarray.cpp
@@ -4,6 +4,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+ bool isPalindrome(string s)
+    {
+        int i = 0, j = s.size() - 1;
+
+        while (i < j)
+        {
+            if (s[i++] != s[j--])
+                return false;
+        }
+        return true;
+    }
+
+    string longestPalindrome(string s)
+    {
+        int n = s.size();
+        if (n == 0)
+            return "";
+
+        if (n == 1)
+            return s;
+
+        string result = "";
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                if (isPalindrome(s.substr(i, j)))
+                {
+                    if (result.size() < j)
+                        result = s.substr(i, j);
+                }
+            }
+        }
+        return result;
+    }
+
 bool isPalindrome(int n){
     int temp = n;
     int num = 0;
